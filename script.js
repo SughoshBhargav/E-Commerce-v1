9f56489e95895e7f38fbe418f7c618633edff7db
SughoshBhargav <sughoshsv@.com>
2023-05-22 07:31:30 +0530

    small changes

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 function sendEmail() {
-    var recipient = 'bestbyu@sales.com';  
-    var bodyVal = document.getElementById('subject');
-    var body = bodyVal.value; 
-    var subject = 'Regarding customer support.';  
-  
-    var gmailUrl = 'https://mail.google.com/mail/?view=cm&fs=1&to=' + encodeURIComponent(recipient) +
-      '&su=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
-  
-    window.open(gmailUrl);
+  var recipient = 'bestbyu@sales.com';
+  var bodyVal = document.getElementById('subject');
+  var body = bodyVal.value;
+  var subject = 'Regarding customer support.';
+
+  var gmailUrl = 'https://mail.google.com/mail/?view=cm&fs=1&to=' + encodeURIComponent(recipient) +
+    '&su=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
+
+  window.open(gmailUrl);
+  setTimeout(function () {
     document.getElementById("successContainer").style.display = "block";
+  }, 1000);
 
-  }
 
-  function closeSuccessMessage() {
-  
+}
 
-    document.getElementById("successContainer").style.display = "none";
+function closeSuccessMessage() {
+  document.getElementById("successContainer").style.display = "none";
 }
